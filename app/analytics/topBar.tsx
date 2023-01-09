f78975b5b1ccb30aed51f68d0caeaa8e566b436d
kaiser24 <damsog38@gmail.com>
2023-01-09 16:00:12 -0500

    Finished the basic view of the top bar

--- app/analytics/topBar.tsx
+++ app/analytics/topBar.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { MdDarkMode } from 'react-icons/md';
+import { IoLanguageSharp } from 'react-icons/io5';
+import { MdViewSidebar } from 'react-icons/md';
 
 const TopBar = () => {
     return (
-        <div>
-            <h1 className=' text-blue-600 '>Top Bar</h1>
+        <div className='bg-[#221c28] h-14 flex items-center'>
+            <div className='w-full flex items-center justify-between px-4'>
+                {/* Left Side */}
+                <div>
+                    <h5 className=" text-2xl cursor-pointer text-gray-300 hover:text-green-600"><MdViewSidebar/></h5>
+                </div>
+                {/* Right Side */}
+                <div className='flex items-center space-x-4'>
+                    <h5 className=" text-2xl cursor-pointer text-gray-300 hover:text-green-600"><MdDarkMode/></h5>
+                    <h5 className=" text-2xl cursor-pointer text-gray-300 hover:text-green-600"><IoLanguageSharp/></h5>
+                    {/* Logo. TODO: Substitute for dropdown */}
+                    <div className='flex items-center'>
+                        <h5 className='text-gray-300 hover:text-green-600 hover:cursor-pointer'>username</h5>
+                        <img  className="h-8 w-8 rounded-full hover:cursor-pointer" src="/avatar_sample.png" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
