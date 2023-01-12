4638e53c911b05962463404450d2ef8ca1aaeef1
kaiser24 <damsog38@gmail.com>
2023-01-11 23:14:49 -0500

    Removed reduntant tags

--- app/analytics/layout.tsx
+++ app/analytics/layout.tsx
@@ -7,17 +7,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html>
-      <body>
-        <div>
-          <TopBar />
-          <div className='relative p-5 my-[2vh] mx-[2vw] md:mx-[8vw] lg:mx-[15vw]'>
-            {/* Div Background */}
-            <div className='absolute z-[-1] rounded-lg top-0 left-0 w-full h-full bg-[#221c28] opacity-95'></div>
-          {children}
-          </div>
+      <>
+        <TopBar />
+        <div className='relative p-5 my-[2vh] mx-[2vw] md:mx-[8vw] lg:mx-[15vw]'>
+          {/* Div Background */}
+          <div className='absolute z-[-1] rounded-lg top-0 left-0 w-full h-full bg-[#221c28] opacity-95'></div>
+        {children}
         </div>
-      </body>
-    </html>
+      </>
   )
 }
