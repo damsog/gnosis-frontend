02fd28011f2825e4cf9a6d2ac8faf0b9cbc2609d
kaiser24 <damsog38@gmail.com>
2023-01-13 23:39:40 -0500

    Starting the rtc connection stream

--- app/main/face-detection/live/page.tsx
+++ app/main/face-detection/live/page.tsx
@@ -1,14 +1,32 @@
+'use client';
+
+import { useState } from 'react';
 import { AiOutlinePlayCircle } from 'react-icons/ai';
 import { BiStopCircle } from 'react-icons/bi';
+import { FaceDetectionStream } from './FaceDetectionStream';
+import VideoElement from './VideoElement';
 
-const FaceDetectionLive = () => {
+function FaceDetectionLive(): any {
+    const faceDetectionStream: FaceDetectionStream = new FaceDetectionStream();
+    const [videoStream, setVideoStream] = useState<MediaStream>();
+
+    const startVideo = async () => {
+        const stream:MediaStream = await faceDetectionStream.start();
+        setVideoStream(stream);
+    };
+
+    const stopVideo = () => {
+        faceDetectionStream.stop();
+    };
+
     return (
     <>
         <div className='flex flex-col'>
-            <video className='border border-green-600 rounded-lg w-full h-full'></video>
+            {videoStream ? 
+                <VideoElement className='border border-green-600 rounded-lg w-full h-full' srcObject={videoStream} autoPlay={true} controls={true}/>
+                : <video className='border border-green-600 rounded-lg w-full h-full'/>}
             <div className='bg-[#2b2532] focus:bg-[#3f3847] rounded-lg p-4 my-4'>
                 <div>
-                    
                 </div>
                 <div className='flex items-center justify-center space-x-16'>
                     <h5 className="hover:text-gray-200 border border-green-700 shadow-lg shadow-green-700/50 active:bg-green-700 rounded-lg px-4 py-1 hover:bg-[#3f3847]
