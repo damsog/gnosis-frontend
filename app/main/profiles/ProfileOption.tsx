8d7efcdd28d6819759950317bcb9084bc68dbf62
kaiser24 <damsog38@gmail.com>
2023-01-21 18:42:10 -0500

    Profile Option card

--- app/main/profiles/ProfileOption.tsx
+++ app/main/profiles/ProfileOption.tsx
@@ -2,21 +2,25 @@ import React from 'react'
 import { IoTrashOutline } from 'react-icons/io5'
 import { GiCheckMark } from 'react-icons/gi'
 import { BsExclamationLg } from 'react-icons/bs'
+import Link from 'next/link'
 
 interface ProfileOptionProps {
+    profileId: string
     name: string
     description: string
     coded: boolean
     className?: string
 }
 
-export default function ProfileOption({name, description, coded, className}:ProfileOptionProps) {
+export default function ProfileOption({profileId, name, description, coded, className}:ProfileOptionProps) {
   return (
-    <div className={`flex flex-row items-center justify-between shadow-md hover:shadow-lg shadow-green-700/50 p-1 rounded-lg 
+    <div className={`flex flex-row items-center justify-between shadow-md shadow-green-700/50 p-1 rounded-lg 
                     bg-[#241f2a] hover:bg-[#2b2532] ${className} group`}>
-        <div>
-            <h5 className='text-gray-300'>{name}</h5>
-            <p className='text-gray-400 font-light text-sm'>{description}</p>
+        <div className='cursor-pointer'>
+            <Link href={`/main/profiles/${profileId}`}>
+              <h5 className='text-gray-300'>{name}</h5>
+              <p className='text-gray-400 font-light text-sm'>{description}</p>
+            </Link>
         </div>
         <div className='flex'>
             <h5 className="invisible group-hover:visible shadow-lg hover:shadow-green-700/50 rounded-lg px-3 py-1 hover:bg-[#3f3847]
