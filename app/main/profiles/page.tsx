238f129cd1d208138e349ab2231ba4b119c9e346
kaiser24 <damsog38@gmail.com>
2023-01-22 16:26:48 -0500

    Merged the profiles page into one

--- app/main/profiles/page.tsx
+++ app/main/profiles/page.tsx
@@ -1,10 +1,40 @@
-import UserProfilesView from "./UserProfilesView";
+'use client';
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import ProfileList from "./ProfileList";
+import { IoAddSharp } from 'react-icons/io5';
 import { Suspense } from "react";
+import NewProfileDialog from "./NewProfileDialog";
 
 const ProfilesPage = () => {
+    //const { data: session, status } = useSession({ required: true });
+    const { data: session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push('/login');
+        },
+    })
+    const router = useRouter();
+
+    if(status === "loading") return <div className="text-green-700">Loading...</div>  
+
     return (
         <>              
-            <Suspense fallback={<div className="text-green-700">Loading...</div>}> <UserProfilesView /> </Suspense>
+            <div className='flex justify-center'>
+                <h5 className='text-gray-300 text-lg'>Profiles</h5>
+            </div>
+            <div className='flex'>
+                <NewProfileDialog userId={session.userId as string} apikey={session.apikey as string} />
+            </div>
+            <div>
+                <hr className="p-2 border-green-700" />
+            </div>
+            <div className=' space-y-4'>
+                <Suspense fallback={<div className="text-green-700">Loading...</div>}>
+                    <ProfileList userId={session.userId as string} apikey={session.apikey as string} />
+                </Suspense>
+            </div>
         </>
     );
 };
