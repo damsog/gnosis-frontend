7c6bf4c23f5f1f38c353404ed5955868e05450cd
kaiser24 <damsog38@gmail.com>
2023-01-23 17:07:17 -0500

    A little bit of refactoring

--- app/main/profiles/ProfileOption.tsx
+++ app/main/profiles/ProfileOption.tsx
@@ -41,12 +41,15 @@ export default function ProfileOption({profileId, name, description, coded, apik
   return (
     <div className={`flex flex-row items-center justify-between shadow-md shadow-green-700/50 p-1 rounded-lg 
                     bg-[#241f2a] hover:bg-[#2b2532] ${className} group`}>
-        <div className='cursor-pointer'>
-            <Link href={`/main/profiles/${profileId}`}>
-              <h5 className='text-gray-300'>{name}</h5>
-              <p className='text-gray-400 font-light text-sm'>{description}</p>
-            </Link>
-        </div>
+        <Link href={`/main/profiles/${profileId}`}>
+            <div className='mx-4 my-2 cursor-pointer flex items-center space-x-4'>
+                <img className='w-10 h-10 rounded-full border border-green-700' src="/no_avatar.webp" alt="" />
+                <div className=''>
+                    <h5 className='text-gray-300'>{name}</h5>
+                    <p className='text-gray-400 font-light text-sm'>{description}</p>
+                </div>
+            </div>
+        </Link>
         <div className='flex'>
             <h5 className="invisible group-hover:visible shadow-lg hover:shadow-green-700/50 rounded-lg px-3 py-1 hover:bg-[#3f3847]
                                  active:translate-y-1 text-xl cursor-pointer text-gray-400 ">{coded ? <GiCheckMark/> : <BsExclamationLg/> }</h5>
