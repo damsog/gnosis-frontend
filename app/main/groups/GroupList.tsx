40ee55bb445a2891b8880a5988efa3b3014cafab
kaiser24 <damsog38@gmail.com>
2023-01-27 13:17:45 -0500

    Group view

---
+++ app/main/groups/GroupList.tsx
@@ -0,0 +1,52 @@
+import { Group } from '@prisma/client';
+import React, { use } from 'react'
+import { useQuery } from 'react-query';
+import GroupOption from './GroupOption';
+
+interface GroupsProps {
+    userId: string
+    apikey: string
+}
+
+const getGroups = async (userId: string, apikey: string) => {
+    try{
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/group/user/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': apikey
+            }})
+        const groups: Group[] = await response.json();
+        return groups;
+    }catch(e){
+        console.log(`Error: ${e}`);
+        return [];
+    }
+}
+
+function GroupList({userId, apikey}: GroupsProps) {
+    // TODO: Remove this once use hook is fixed
+    //const profiles = use( getProfiles(userId, apikey) );
+    const query = useQuery(["groups",userId, apikey], () => getGroups(userId, apikey) )
+    if (query.isLoading) {
+      return <h2>Loading...</h2>;
+    }
+
+
+    return (
+        <>
+            {query.data!.map( (group) => (
+                <div key={group.id}>
+                <GroupOption
+                    groupId={group.id}
+                    name={group.name}
+                    description={group.description!}
+                    dataset={group.dataset!}
+                    apikey={apikey} 
+                />
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default GroupList;
--- app/main/groups/page.tsx
+++ app/main/groups/page.tsx
@@ -1,9 +1,40 @@
+'use client';
+
 import type { NextPage } from 'next'
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { Suspense } from 'react';
+import GroupList from './GroupList';
+import NewGroupDialog from './NewGroupDialog';
 
 const GroupsPage = () => {
+    //const { data: session, status } = useSession({ required: true });
+    const { data: session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push('/login');
+        },
+    })
+    const router = useRouter();
+
+    if(status === "loading") return <div className="text-green-700">Loading...</div>  
+
     return (
-        <div>
-            <h1 className=' text-blue-600 '>Groups view</h1>
+        <div className="bg-[#221c28]">              
+            <div className='flex justify-center'>
+                <h5 className='text-gray-300 text-lg'>Groups</h5>
+            </div>
+            <div className='flex'>
+                <NewGroupDialog userId={session.userId as string} apikey={session.apikey as string} />
+            </div>
+            <div>
+                <hr className="p-2 border-green-700" />
+            </div>
+            <div className=' space-y-4'>
+                <Suspense fallback={<div className="text-green-700">Loading...</div>}>
+                    <GroupList userId={session.userId as string} apikey={session.apikey as string} />
+                </Suspense>
+            </div>
         </div>
     );
 };
