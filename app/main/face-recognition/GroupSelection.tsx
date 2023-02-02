94aa4895cfd65b50d825ad8d00fc277f08f022ba
kaiser24 <damsog38@gmail.com>
2023-02-01 22:48:40 -0500

    Added the group selector for the dataset to use as recognizer base

---
+++ app/main/face-recognition/GroupSelection.tsx
@@ -0,0 +1,98 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+//import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import {AiOutlineCheck, AiOutlineDown } from 'react-icons/ai'
+import { Group } from '@prisma/client'
+import { useQuery } from 'react-query'
+import FaceDetectionSnap from './FaceRecognitionLive'
+import FaceDetectionLive from './FaceRecognitionSnap'
+
+const getCodedGroups = async (userId: string, apikey: string) => {
+  try{
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/group/user/coded/${userId}`, {
+          method: 'GET',
+          headers: {
+              'Authorization': apikey
+          }})
+      const groups: Group[] = await response.json();
+      return groups;
+  }catch(e){
+      console.log(`Error: ${e}`);
+      return [];
+  }
+}
+
+interface GroupSelectionProps {
+  option: string;
+  userId: string;
+  apikey: string;
+}
+
+export default function GroupSelection({option, userId, apikey}: GroupSelectionProps) {
+    const query = useQuery(["coded-groups", userId, apikey], () => getCodedGroups(userId, apikey) );
+    const [selected, setSelected] = useState<Group | undefined>(undefined)
+    
+    // List of the coded available groups
+    if(query.isLoading) return <div className="text-green-700">Loading...</div>
+
+  return (
+    <div>
+    <div className="top-16 w-72">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-[#2b2532] hover:bg-[#3f3847] text-gray-300 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected ? selected.name : "Select A Group"}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <AiOutlineDown
+                className="h-4 w-4 text-gray-400 text-xs"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-[#2b2532] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {query.data!.map((group, groupIdx) => (
+                <Listbox.Option
+                  key={groupIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-[#3f3847] text-green-700' : 'text-gray-300'
+                    }`
+                  }
+                  value={group}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {group.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-700">
+                          <AiOutlineCheck className="h-4 w-4" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+    <div>
+          {option === "live" && <FaceDetectionLive groupId={selected?.dataset ? selected.dataset : ""}/>}
+          {option === "snap" && <FaceDetectionSnap groupId={selected?.dataset ? selected.dataset : ""}/>}
+    </div>
+    </div>
+  )
+}
--- app/main/face-recognition/faceRecognitionSelector.tsx
+++ app/main/face-recognition/faceRecognitionSelector.tsx
@@ -3,33 +3,48 @@
 import type { NextPage } from 'next'
 import { useState } from 'react';
 import Link from 'next/link';
+import GroupSelection from './GroupSelection';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import FaceDetectionLive from './FaceRecognitionLive';
+import FaceDetectionSnap from './FaceRecognitionSnap';
 
 const FaceRecognitionSelector: NextPage = () => {
-    const [ option, setOption ] = useState<string>();
+    const [ option, setOption ] = useState<string>("live");
+    const { data: session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push('/login');
+        },
+    })
+    const router = useRouter();
+    
+    if(status === "loading") return <div className="text-green-700">Loading...</div> 
     
     const selectOption = (option:string) => {
         setOption(option);
     }
 
     return (
-        <>
+        <div>
             <div className='flex justify-center'>
                 <h5 className='text-gray-300 text-lg'>Face Recognition 
                     {option === "live" && <span className='rounded-md p-1 bg-green-700 font-light'>Live</span>}
                     {option === "snap" && <span className='rounded-md p-1 bg-green-700 font-light'>Snap</span>}
                 </h5>
             </div>
-            <div className='mx-1'>
-                    <Link rel="stylesheet" href={`/main/face-recognition/live`}>
-                        <button className={`text-gray-400 hover:text-gray-200 border border-green-700 shadow-lg shadow-green-700/50 rounded-lg px-4 py-2 
-                                ${option === "live" ? "bg-green-700 text-gray-200" : "bg-[#2b2532] hover:bg-[#3f3847]"}`} onClick={()=>selectOption("live")}>Live</button>
-                    </Link>
-                    <Link rel="stylesheet" href={`/main/face-recognition/snap`}>
-                        <button className={`text-gray-400 hover:text-gray-200 border border-green-700 shadow-lg shadow-green-700/50 rounded-lg px-4 py-2 
-                                ${option === "snap" ? "bg-green-700 text-gray-200" : "bg-[#2b2532] hover:bg-[#3f3847]"}`} onClick={()=>selectOption("snap")}>Snap</button>
-                    </Link>
+            <div className='relative'>
+                <div className='absolute top-0 right-0'>
+                    <div className='mx-1'>
+                                <button className={`text-gray-400 hover:text-gray-200 border border-green-700 shadow-lg shadow-green-700/50 rounded-lg px-4 py-1
+                                        ${option === "live" ? "bg-green-700 text-gray-200" : "bg-[#2b2532] hover:bg-[#3f3847]"}`} onClick={()=>selectOption("live")}>Live</button>
+                                <button className={`text-gray-400 hover:text-gray-200 border border-green-700 shadow-lg shadow-green-700/50 rounded-lg px-4 py-1 
+                                        ${option === "snap" ? "bg-green-700 text-gray-200" : "bg-[#2b2532] hover:bg-[#3f3847]"}`} onClick={()=>selectOption("snap")}>Snap</button>
+                    </div>
+                </div>
+                <GroupSelection option={option} userId={session.userId as string} apikey={session.apikey as string}/>
             </div>
-        </>
+        </div>
     );
 };
 
